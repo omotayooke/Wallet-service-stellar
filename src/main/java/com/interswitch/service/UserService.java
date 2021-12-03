4b14e118d297f4ee9f413c7ae3725857cc6bcd68
omotayooke <52256670+omotayooke@users.noreply.github.com>
2021-12-03 14:54:29 +0100

    Delete src/main directory

--- src/main/java/com/interswitch/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.interswitch.service;
-
-import com.interswitch.model.User;
-
-public interface UserService {
-	public User findUserByEmail(String email);
-
-	public void saveUser(User user);
-}
